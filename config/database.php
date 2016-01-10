57e068e94e6012283613a70b2fc0875fccac5a99
jdmcpeek <david.mcpeek@yale.edu>
2016-01-10 15:36:27 -0800

    new db

--- config/database.php
+++ config/database.php
@@ -1,12 +1,12 @@
 <?php
 // comment for dev, uncomment for prod
 // 
-// $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-// $host = $url["host"];
-// $username = $url["user"];
-// $password = $url["pass"];
-// $database = substr($url["path"], 1);
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 
 return [
 
@@ -61,30 +61,30 @@ return [
         ],
 
         // production
-        // 'mysql' => [
-        //     'driver'    => 'mysql',
-        //     'host'      => $host,
-        //     'database'  => $database,
-        //     'username'  => $username,
-        //     'password'  => $password,
-        //     'charset'   => 'utf8',
-        //     'collation' => 'utf8_unicode_ci',
-        //     'prefix'    => '',
-        //     'strict'    => false,
-        // ],
-
-        // development
-          'mysql' => [
+        'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', '127.0.0.1'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => $host,
+            'database'  => $database,
+            'username'  => $username,
+            'password'  => $password,
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
         ],
+
+        // development
+        //   'mysql' => [
+        //     'driver'    => 'mysql',
+        //     'host'      => env('DB_HOST', '127.0.0.1'),
+        //     'database'  => env('DB_DATABASE', 'forge'),
+        //     'username'  => env('DB_USERNAME', 'forge'),
+        //     'password'  => env('DB_PASSWORD', ''),
+        //     'charset'   => 'utf8',
+        //     'collation' => 'utf8_unicode_ci',
+        //     'prefix'    => '',
+        //     'strict'    => false,
+        // ],
 
         'pgsql' => [
             'driver'   => 'pgsql',
